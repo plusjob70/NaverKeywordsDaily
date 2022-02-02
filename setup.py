e2afc4d6252de93ce71417f4637c749db2e0ba37
plusjob70 <plusjob70@naver.com>
2022-02-02 11:06:33 +0900

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='naver_trends',
+    version='1.0',
+    description='네이버 키워드별 일간검색수를 계산 후 BigQuery 테이블에 삽입',
+    author='junho Lee',
+    author_email='plusjob70@gmail.com',
+    packages=['naver_trends']
+    
+)
