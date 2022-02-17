9822c408ee7c1547a95bfd949376a6c370ef1fc1
plusjob70 <plusjob70@naver.com>
2022-02-17 20:37:28 +0900

    split main to basic_main, gender_main

--- naver_trends/basic_main.py
+++ naver_trends/basic_main.py
@@ -1,200 +1,132 @@
-#! python
+#! /usr/bin/env python
+import sys
 import pandas as pd
-from time import sleep
-from naver_trends.common.constant import IS_DANGEROUS_TIME
+from naver_trends.common.constant import IS_DANGEROUS_TIME, MAX_ANAL_BATCH_SIZE, MIN_INSERT_BATCH_SIZE, BIGQUERY_CHUNK_SIZE
 from naver_trends.keywordanal.keywordanal import Keywordanal
-from naver_trends.service.gmailservice import GmailService
 from naver_trends.service.bigqueryservice import BigQueryService
 from naver_trends.service.gsheetsservice import GSheetsService
-from google.cloud.exceptions import NotFound
+from naver_trends.service.gmailservice import GmailService
 
+MODE = 'basic'
 
-if __name__ == '__main__':
-    msg    = []
-    text   = ''
-    status = 'succeeded'
+# check dangerous time
+if IS_DANGEROUS_TIME: 
+    sys.exit('Deny access to the server. It is a dangerous time.')
 
-    # create GmailService instance
-    #gmail  = GmailService()
+# check length of argument
+if len(sys.argv) < 2:
+    sys.exit("There are no selected items")
 
-    # check dangerous time
-    if (IS_DANGEROUS_TIME):
-        text = 'Deny access to the server. It is a dangerous time.'
-        print(text)
-        #gmail.send_message(gmail.create_message(text, 'failed'))
-        exit()
+# extract selected_client_list
+selected_client_name_list = sys.argv[1:]
 
-    # BigQuery table schema
-    schema = {
-        'corporate_id': 'STRING',
-        'brand_id'    : 'STRING',
-        'date'        : 'STRING',
-        'keyword'     : 'STRING',
-        'keyword_type': 'STRING',
-        'category_1'  : 'STRING',
-        'category_2'  : 'STRING',
-        'category_3'  : 'STRING',
-        'category_4'  : 'STRING',
-        'category_5'  : 'STRING',
-        'device_type' : 'STRING',
-        'queries'     : 'INTEGER'
-    }
-    # dataframe columns
-    df_columns   = schema.keys()
+# BigQuery table schema
+schema = {
+    'corporate_id': 'STRING',
+    'brand_id'    : 'STRING',
+    'keyword'     : 'STRING',
+    'keyword_type': 'STRING',
+    'category_1'  : 'STRING',
+    'category_2'  : 'STRING',
+    'category_3'  : 'STRING',
+    'category_4'  : 'STRING',
+    'category_5'  : 'STRING',
+    'date'        : 'STRING',
+    'queries'     : 'INTEGER',
+    'device_type' : 'STRING'
+}
 
-    # create BigQueryService & GSheetsService & keyword anal tool instance
-    bigquery     = BigQueryService(schema=schema)
-    gsheets      = GSheetsService()
-    keyword_anal = Keywordanal()
+# create BigQueryService & GSheetsService & GmailService & keyword analize tool & DataFrame instance
+bigquery     = BigQueryService(schema=schema, mode=MODE)
+gsheets      = GSheetsService()
+gmail        = GmailService()
+keyword_anal = Keywordanal()
 
-    # get client file info (id, name)
-    client_info_dict = gsheets.get_all_files_info()
+dataframe_list = []
+dataframe_cols = schema.keys()
+dataframe_rows = 0
+insert_results = []
 
-    print('Start to analyze keywords...', flush=True)
-    for client in client_info_dict:
-        client_id   = client['id']
-        client_name = client['name']
-        text        = 'Analyzing keywords for client : {}'.format(client_name)
-        msg.append(text)
-        print(text, flush=True)
+print(gmail.write_message(f'mode : {MODE}'), flush=True)
 
-        # get sheet file
-        sheet = gsheets.get_sheet(id=client_id)
+# get all clients infomation 
+# client_info_list = list[dict{id, name}, dict{id, name}, ...]
+client_info_list = gsheets.get_all_files_info()
+print('Start to analyze keywords...', flush=True)
+for client in client_info_list:
+    client_id   = client['id']
+    client_name = client['name']
 
-        # preprocess sheet data
-        for idx in range(len(sheet)):
-            sheet[idx]['keyword'] = sheet[idx]['keyword'].replace(' ', '').upper()
-        sheet_data_list = [sheet[i:i+5] for i in range(0, len(sheet), 5)]
+    if client_name not in selected_client_name_list:
+        continue
+
+    print(gmail.write_message(f'Analyzing keywords for client : {client_name}'), flush=True)
+
+    # get sheet file
+    sheet      = gsheets.get_sheet(id=client_id)
+    sheet_size = len(sheet)
+
+    # preprocess sheet data
+    for idx in range(sheet_size):
+        sheet[idx]['keyword'] = sheet[idx]['keyword'].replace(' ', '').upper()
+
+    # get table infomation
+    table = bigquery.get_table_info(client_name=client_name)
+
+    # set latest date
+    keyword_anal.latest_date_dict = bigquery.get_latest_date_dict(client_name=client_name)
+    
+    for point in range(0, sheet_size, MAX_ANAL_BATCH_SIZE):
+        sheet_chunck_list = sheet[point:point+MAX_ANAL_BATCH_SIZE]
+        keyword_list = list(map(lambda row: row['keyword'], sheet_chunck_list))
+        print(keyword_list, flush=True)
+
+        keyword_dict = keyword_anal.get_results(keyword_list=keyword_list)
+
+        for idx, row in enumerate(sheet_chunck_list):
+            pc_data_size = len(keyword_dict[keyword_list[idx]]['dpc'].keys())
+            if pc_data_size > 0:
+                row['date']        = keyword_dict[keyword_list[idx]]['dpc'].keys()
+                row['queries']     = keyword_dict[keyword_list[idx]]['dpc'].values()
+                row['device_type'] = 'PC'
+                dataframe_list.append(pd.DataFrame(columns=dataframe_cols, data=sheet_chunck_list[idx]))
+
+            mo_data_size = len(keyword_dict[keyword_list[idx]]['dmc'].keys())
+            if mo_data_size > 0:
+                row['date']        = keyword_dict[keyword_list[idx]]['dmc'].keys()
+                row['queries']     = keyword_dict[keyword_list[idx]]['dmc'].values()
+                row['device_type'] = '모바일'
+                dataframe_list.append(pd.DataFrame(columns=dataframe_cols, data=sheet_chunck_list[idx]))
+            dataframe_rows = dataframe_rows + pc_data_size + mo_data_size
         
-        # get table infomation
-        table = bigquery.get_table_info(client_name)
+        if dataframe_rows > MIN_INSERT_BATCH_SIZE:
+            insert_results = bigquery.client.insert_rows_from_dataframe(
+                table=table,
+                dataframe=pd.concat(dataframe_list),
+                selected_fields=bigquery.table_schema,
+                chunk_size=BIGQUERY_CHUNK_SIZE
+            ) 
+            dataframe_rows = 0
+            dataframe_list = []
+            if sum(map(lambda x: len(x), insert_results)) != 0:
+                print(insert_results)
+                print(gmail.write_message(f"insertion failed : {keyword_list}"), flush=True)
+                gmail.tag = 'failed'
 
-        # find latest date
-        num_rows, s_result = bigquery.get_latest_date_dict(client_name)
+    if dataframe_rows > 0:
+        insert_results = bigquery.client.insert_rows_from_dataframe(
+            table=table,
+            dataframe=pd.concat(dataframe_list),
+            selected_fields=bigquery.table_schema,
+            chunk_size=BIGQUERY_CHUNK_SIZE
+        )
+        dataframe_rows = 0
+        dataframe_list = []
 
-        # update table
-        i_result = None
-        if (num_rows != 0):
-            latest_date_dict = {}
-            for row in s_result:
-                if row.device_type not in latest_date_dict:
-                    latest_date_dict[row.device_type] = {}
-                latest_date_dict[row.device_type][row.keyword] = row.latest_date
+    if sum(map(lambda x: len(x), insert_results)) != 0:
+        print(gmail.write_message(f'{client_name} failed.'), flush=True)
+        gmail.tag = 'failed'
+    else:
+        print(gmail.write_message(f'{client_name} Done.'), flush=True)
 
-            keyword_anal.latest_date_dict = latest_date_dict
-
-            df = pd.DataFrame(columns=df_columns)
-            for chunk in sheet_data_list:
-                keyword_list = [row.get('keyword', None) for row in chunk]
-                keyword_dict = keyword_anal.get_keyword_anal_results(keyword_list)
-                print(keyword_list, flush=True)
-
-                for idx, row in enumerate(chunk):
-                    pc_data = {
-                        'corporate_id': row['corporate_id'],
-                        'brand_id'    : row['brand_id'],
-                        'date'        : keyword_dict[keyword_list[idx]]['dpc'].keys(),
-                        'keyword'     : row['keyword'],
-                        'keyword_type': row['keyword_type'],
-                        'category_1'  : row['category_1'],
-                        'category_2'  : row['category_2'],
-                        'category_3'  : row['category_3'],
-                        'category_4'  : row['category_4'],
-                        'category_5'  : row['category_5'],
-                        'device_type' : 'PC',
-                        'queries'     : keyword_dict[keyword_list[idx]]['dpc'].values()
-                    }
-                    mo_data = {
-                        'corporate_id': row['corporate_id'],
-                        'brand_id'    : row['brand_id'],
-                        'date'        : keyword_dict[keyword_list[idx]]['dmc'].keys(),
-                        'keyword'     : row['keyword'],
-                        'keyword_type': row['keyword_type'],
-                        'category_1'  : row['category_1'],
-                        'category_2'  : row['category_2'],
-                        'category_3'  : row['category_3'],
-                        'category_4'  : row['category_4'],
-                        'category_5'  : row['category_5'],
-                        'device_type' : '모바일',
-                        'queries'     : keyword_dict[keyword_list[idx]]['dmc'].values()
-                    }
-                    df = pd.concat([df, pd.DataFrame(pc_data), pd.DataFrame(mo_data)])
-
-            if (df.empty):
-                text = '"{}" No change in data'.format(client_name) 
-                print(text, flush=True)
-                msg.append(text)
-                continue
-            else:
-                print(df, flush=True)
-                print('Inserting to BigQuery table...', flush=True)
-                i_result = bigquery.client.insert_rows_from_dataframe(table=table, dataframe=df, selected_fields=bigquery.table_schema)
-
-        else:
-            print('New table found.', flush=True)
-            time_out = 0
-            keyword_anal.latest_date_dict = {}
-
-            for chunk in sheet_data_list:
-                keyword_list = [row.get('keyword', None) for row in chunk]
-                keyword_dict = keyword_anal.get_keyword_anal_results(keyword_list)
-                print(keyword_list, flush=True)
-
-                df = pd.DataFrame(columns=df_columns)
-                for idx, row in enumerate(chunk):
-                    pc_data = {
-                        'corporate_id': row['corporate_id'],
-                        'brand_id'    : row['brand_id'],
-                        'date'        : keyword_dict[keyword_list[idx]]['dpc'].keys(),
-                        'keyword'     : row['keyword'],
-                        'keyword_type': row['keyword_type'],
-                        'category_1'  : row['category_1'],
-                        'category_2'  : row['category_2'],
-                        'category_3'  : row['category_3'],
-                        'category_4'  : row['category_4'],
-                        'category_5'  : row['category_5'],
-                        'device_type' : 'PC',
-                        'queries'     : keyword_dict[keyword_list[idx]]['dpc'].values()
-                    }
-                    mo_data = {
-                        'corporate_id': row['corporate_id'],
-                        'brand_id'    : row['brand_id'],
-                        'date'        : keyword_dict[keyword_list[idx]]['dmc'].keys(),
-                        'keyword'     : row['keyword'],
-                        'keyword_type': row['keyword_type'],
-                        'category_1'  : row['category_1'],
-                        'category_2'  : row['category_2'],
-                        'category_3'  : row['category_3'],
-                        'category_4'  : row['category_4'],
-                        'category_5'  : row['category_5'],
-                        'device_type' : '모바일',
-                        'queries'     : keyword_dict[keyword_list[idx]]['dmc'].values()
-                    }
-                    df = pd.concat([df, pd.DataFrame(pc_data), pd.DataFrame(mo_data)])
-                print('Inserting to BigQuery table...', flush=True)
-
-                while (True):
-                    try:
-                        i_result = bigquery.client.insert_rows_from_dataframe(table=table, dataframe=df, selected_fields=bigquery.table_schema, chunk_size=1000)
-                        break
-                    except NotFound:
-                        print('not found table because of delay. please wait...', flush=True)
-                        time_out += 1
-                        sleep(0.5)
-                        if (time_out > 50):
-                            text = '"{}" BigQuery table not found'.format(client_name)
-                            print(text, flush=True)
-                            #gmail.send_message(gmail.create_message(text, 'failed'))
-                            exit()
-                        continue
-
-        if (type(i_result) is list):
-            text = '{} Done'.format(client_name)
-        else:
-            text = '{} Failed'.format(client_name)
-            status = 'failed'
-        print(text, flush = True)
-        msg.append(text)
-
-    msg ='\n'.join(msg)
-    #gmail.send_message(gmail.create_message(msg, status))
+#gmail.send_message()
--- naver_trends/gender_main.py
+++ naver_trends/gender_main.py
@@ -1,4 +1,134 @@
+#! /usr/bin/env python
 import sys
+import pandas as pd
+from naver_trends.common.constant import IS_DANGEROUS_TIME, MAX_ANAL_BATCH_SIZE, MIN_INSERT_BATCH_SIZE, BIGQUERY_CHUNK_SIZE
+from naver_trends.keywordanal.genderanal import Genderanal
+from naver_trends.service.bigqueryservice import BigQueryService
+from naver_trends.service.gsheetsservice import GSheetsService
+from naver_trends.service.gmailservice import GmailService
 
-if __name__ == '__main__':
-    print(sys.argv[0])
+MODE = 'gender'
+
+# check dangerous time
+if IS_DANGEROUS_TIME: 
+    sys.exit('Deny access to the server. It is a dangerous time.')
+
+# check length of argument
+if len(sys.argv) < 2:
+    sys.exit("There are no selected items")
+
+# extract selected_client_list
+selected_client_name_list = sys.argv[1:]
+
+# BigQuery table schema
+schema = {
+    'corporate_id': 'STRING',
+    'brand_id'    : 'STRING',
+    'keyword'     : 'STRING',
+    'keyword_type': 'STRING',
+    'category_1'  : 'STRING',
+    'category_2'  : 'STRING',
+    'category_3'  : 'STRING',
+    'category_4'  : 'STRING',
+    'category_5'  : 'STRING',
+    'date'        : 'STRING',
+    'queries'     : 'INTEGER',
+    'gender'      : 'STRING'
+}
+
+# create BigQueryService & GSheetsService & GmailService & keyword analize tool & DataFrame instance
+bigquery     = BigQueryService(schema=schema, mode=MODE)
+gsheets      = GSheetsService()
+gmail        = GmailService()
+keyword_anal = Genderanal()
+
+dataframe_list = []
+dataframe_cols = schema.keys()
+dataframe_rows = 0
+insert_results = []
+
+print(gmail.write_message(f'mode : {MODE}'), flush=True)
+
+# get all clients infomation 
+# client_info_list = list[dict{id, name}, dict{id, name}, ...]
+client_info_list = gsheets.get_all_files_info()
+print('Start to analyze keywords...', flush=True)
+for client in client_info_list:
+    client_id   = client['id']
+    client_name = client['name']
+
+    if client_name not in selected_client_name_list:
+        continue
+
+    print(gmail.write_message(f'Analyzing keywords for client : {client_name}'), flush=True)
+
+    # get sheet file
+    sheet      = gsheets.get_sheet(id=client_id)
+    sheet_size = len(sheet)
+
+    # preprocess sheet data
+    for idx in range(sheet_size):
+        sheet[idx]['keyword'] = sheet[idx]['keyword'].replace(' ', '').upper()
+
+    # get table infomation
+    table = bigquery.get_table_info(client_name=client_name)
+
+    # set latest date
+    keyword_anal.latest_date_dict = bigquery.get_latest_date_dict(client_name=client_name)
+    
+    for point in range(0, sheet_size, MAX_ANAL_BATCH_SIZE):
+        sheet_chunck_list = sheet[point:point+MAX_ANAL_BATCH_SIZE]
+        keyword_list = list(map(lambda row: row['keyword'], sheet_chunck_list))
+        print(keyword_list, flush=True)
+
+        keyword_dict = keyword_anal.get_results(keyword_list=keyword_list)
+
+        for idx, row in enumerate(sheet_chunck_list):
+            pc_data_size = len(keyword_dict[keyword_list[idx]]['dmc'].keys())
+            if pc_data_size > 0:
+                row['date']        = keyword_dict[keyword_list[idx]]['dmc'].keys()
+                row['queries']     = keyword_dict[keyword_list[idx]]['dmc'].values()
+                row['gender']      = '남'
+                dataframe_list.append(pd.DataFrame(columns=dataframe_cols, data=sheet_chunck_list[idx]))
+
+            mo_data_size = len(keyword_dict[keyword_list[idx]]['dwc'].keys())
+            if mo_data_size > 0:
+                row['date']        = keyword_dict[keyword_list[idx]]['dwc'].keys()
+                row['queries']     = keyword_dict[keyword_list[idx]]['dwc'].values()
+                row['gender']      = '여'
+                dataframe_list.append(pd.DataFrame(columns=dataframe_cols, data=sheet_chunck_list[idx]))
+            dataframe_rows = dataframe_rows + pc_data_size + mo_data_size
+        
+        if dataframe_rows > MIN_INSERT_BATCH_SIZE:
+            insert_results = bigquery.client.insert_rows_from_dataframe(
+                table=table,
+                dataframe=pd.concat(dataframe_list),
+                selected_fields=bigquery.table_schema,
+                chunk_size=BIGQUERY_CHUNK_SIZE
+            ) 
+            dataframe_rows = 0
+            dataframe_list = []
+            if sum(map(lambda x: len(x), insert_results)) != 0:
+                print(insert_results)
+                print(gmail.write_message(f"insertion failed : {keyword_list}"), flush=True)
+                gmail.tag = 'failed'
+
+    if dataframe_rows > 0:
+        insert_results = bigquery.client.insert_rows_from_dataframe(
+            table=table,
+            dataframe=pd.concat(dataframe_list),
+            selected_fields=bigquery.table_schema,
+            chunk_size=BIGQUERY_CHUNK_SIZE
+        )
+        dataframe_rows = 0
+        dataframe_list = []
+
+    if sum(map(lambda x: len(x), insert_results)) != 0:
+        print(gmail.write_message(f'{client_name} failed.'), flush=True)
+        gmail.tag = 'failed'
+    else:
+        print(gmail.write_message(f'{client_name} Done.'), flush=True)
+
+#gmail.send_message()
+            
+            
